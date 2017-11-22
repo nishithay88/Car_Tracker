c7cf79553b4634eff0a49cc8b231448dba2c18d5
nishithay88 <nishitha.y@outlook.com>
2017-11-21 19:30:29 -0500

    Add Application

---
+++ api/src/main/java/Application.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class Application {
+}
