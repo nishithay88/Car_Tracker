6dd24d068898d9929f231a4b1ae253efdfd2aafd
nishithay88 <nishitha.y@outlook.com>
2017-11-21 19:29:54 -0500

    Add AppInitializer

---
+++ api/src/main/java/AppInitializer.java
@@ -0,0 +1,15 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Application.class,JPAConfig.class,Cors.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/api/*"};
+    }
+}
